768c375398d6d55062d2c45b671113fae6d04cb1
Sjors Wijsman <sjorswijsman@gmail.com>
2023-06-29 23:26:34 +0200

    added home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
 <h1>WebRTC without signaling server</h1>
+<hr />
+<p>
+	Connect with a peer on the same network by creating an <a href="offering">offer</a> and letting
+	them <a href="answer">answer</a>.
+</p>
